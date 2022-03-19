87ea01f2feb86bb806fa5045bccf721aefd5abb8
Nguyen Nhut Tin <nngu0068@student.monash.edu>
2022-03-19 15:42:27 +1100

    a

---
+++ cycleGAN3rd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --account=zd26
+#SBATCH --job-name=cycle_gan_3rdloss_wandb
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --partition=desktop
+#SBATCH --qos=desktopq
+#SBATCH --ntasks=8
+#SBATCH --mem=50G
+#SBATCH --mail-user=tinplay41@gmail.com
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --output=/fs02/zd26/collage_main/Tin/pytorch-CycleGAN-and-pix2pix/output_folder/sbatch_out.out
+#SBATCH --error=/fs02/zd26/collage_main/Tin/pytorch-CycleGAN-and-pix2pix/output_folder/sbatch_err.err
+
+
+# A script to monitor gpu and cpu usage for arbitrary commands (Python flavour?) 
+pathToRepo=/fs02/zd26/collage_main/Tin/pytorch-CycleGAN-and-pix2pix/
+# Define BASH functions for monitoring 
+
+echo 'train pix-to-pix'
+#module load cuda/10.1
+module load pytorch
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+echo $(which python3)
+
+cd $pathToRepo
+#wandb login myAPI
+python train.py --dataroot ./datasets/collage_testing --name collage_test_cycle_gan_3rdloss_wandb --model cycle_gan_3rdloss --direction AtoB --dataset_mode unaligned  --use_wandb --continue_train --epoch_count 81
+
+
+echo 'Run finished!'
---
+++ trying.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --account=zd26
+#SBATCH --job-name=cycle_gan_3rdloss
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --partition=desktop
+#SBATCH --qos=desktopq
+#SBATCH --ntasks=8
+#SBATCH --mem=50G
+#SBATCH --mail-user=tinplay41@gmail.com
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --output=sbatch_out.out
+#SBATCH --error=sbatch_err.err
+
+
+# A script to monitor gpu and cpu usage for arbitrary commands (Python flavour?) 
+pathToRepo=/home/nngu0068/zd26/collage_main/Tin/pytorch-CycleGAN-and-pix2pix
+# Define BASH functions for monitoring 
+
+echo 'train pix-to-pix'
+#module load cuda/10.1
+module load pytorch
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+#module load anaconda/2020.07-Python3.8-gcc8
+#conda create -n myconda python=3.8 anaconda
+#source activate myconda
+# module load anaconda/2019.03-Python3.7-gcc5
+#source /scratch/vf38/jtos0003/miniconda/bin/activate 
+#conda activate pytorch-CycleGAN-and-pix2pix
+echo $(which python3)
+
+cd $pathToRepo
+# python3 -m cProfile -o output/replicate_umm.prof UMM_discovery_BBBC021_copy.py
+#python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA --use_wandb
+#python train.py --dataroot ./datasets/collage_testing --name collage_test_pix2pix --model pix2pix --direction AtoB --dataset_mode unaligned --use_wandb
+#wandb login myAPI
+#wandb offline
+python -m visdom.server
+python train.py --dataroot ./datasets/collage_testing --name collage_test_cycle_gan_3rdloss --model cycle_gan_3rdloss --direction AtoB --dataset_mode unaligned #--use_wandb 
+#python train.py --dataroot ./datasets/john_multiple_match --name abc --model pix2pix --direction BtoA --use_wandb
+
+echo 'Run finished!'
